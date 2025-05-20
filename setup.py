21eb358dcbc1bd7813497603f9ccc400628f5b50
Furkan <141925837+CreativexName@users.noreply.github.com>
2025-05-21 00:26:50 +0200

    changed internal setup package name, should work, hopefully :')'

--- setup.py
+++ setup.py
@@ -3,19 +3,18 @@ from setuptools import setup, find_packages
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
-
 setup(
     name="countsy",
-    version="1.0.2",
+    version="1.0.3",
     author="Furkan Tandogan",
     description="Count lines of Python code in directories",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(where="."),
+    package_dir={"countsy": "src"},
     entry_points={
         "console_scripts": [
-            "countsy=src.cli:main"
+            "countsy=countsy.cli:main"
         ],
     },
     python_requires=">=3.6",
