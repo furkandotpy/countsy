fad7d55e8efcb065ad90e9ad02a6f3208e8b8472
Furkan <141925837+CreativexName@users.noreply.github.com>
2025-05-20 23:33:27 +0200

    Second commit. Worked on Distro dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 from setuptools import setup
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
 setup(
     name="pycount",
     version="1.0.0",
-    package_dir={'': 'src'},
-    packages=[''],
+    author="Furkan Tandogan",
+    description="Count lines of Python code in directories",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     entry_points={
         "console_scripts": [
-            "pycount = cli:main"
+            "pycount=cli:main"
         ],
     },
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
